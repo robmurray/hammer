e5da4c2cac509d406ac44d890ae2fb5a47d3791d
rob <tebor@wildmonkeybytes.com>
2015-04-03 10:41:30 -0400

    init checkin

--- src/main/java/com/wmb/hammer.java
+++
@@ -1,7 +0,0 @@
-package com.wmb;
-
-/**
- * Created by rob on 4/3/15.
- */
-public class hammer {
-}
---
+++ src/main/java/com/wmb/hammer/LoadManager.java
@@ -0,0 +1,7 @@
+package com.wmb.hammer;
+
+/**
+ * Created by rob on 4/3/15.
+ */
+public class LoadManager {
+}
